dc29debe911704d05bf24b796d336dbc5e4bd51e
Sukmin Kang <sukminkang92@gmail.com>
2023-08-01 20:39:47 +0900

    variant init

--- main.cpp
+++ main.cpp
@@ -1,11 +1,84 @@
 #include "mbed.h"
+#include "COMPONENT_SD/include/SD/SDBlockDevice.h"
+
+// Weak empty variant initialization function.
+// May be redefined by variant files.
+void initVariant() __attribute__((weak));
+void initVariant() { }
+
+// Instantiate the SDBlockDevice by specifying the SPI pins connected to the SDCard
+// socket. The PINS are:
+//     MOSI (Master Out Slave In)
+//     MISO (Master In Slave Out)
+//     SCLK (Serial Clock)
+//     CS (Chip Select)
+SDBlockDevice sd(D11, D12, A7, A6);
+uint8_t block[512] = "Hello World!\n";
+
+int sdInit()
+{
+	// Call the SDBlockDevice instance initialisation method
+    if (0 != sd.init()) {
+        printf("Init failed \n");
+        return -1;
+    }
+
+	printf("sd size: %llu\n",         sd.size());
+    printf("sd read size: %llu\n",    sd.get_read_size());
+    printf("sd program size: %llu\n", sd.get_program_size());
+    printf("sd erase size: %llu\n",   sd.get_erase_size());
+
+    // Set the frequency
+    if (0 != sd.frequency(4000000)) {
+        printf("Error setting frequency \n");
+    }
+
+    if (0 != sd.erase(0, sd.get_erase_size())) {
+        printf("Error Erasing block \n");
+    }
+
+    // Write data block to the device
+    if (0 == sd.program(block, 0, 512)) {
+        // Read the data block from the device
+        if (0 == sd.read(block, 0, 512)) {
+            // Print the contents of the block
+            printf("%s", block);
+        }
+    }
+
+    // Call the SDBlockDevice instance de-initialisation method
+    sd.deinit();
+
+	return 0;
+}
 
 int main()
 {
+	initVariant();
+
+	sdInit();
+
+	int cnt = 0;
+	DigitalOut main_led(LED1);
+	DigitalOut red_led(LED2);
+	DigitalOut green_led(LED3);
+	DigitalOut blue_led(LED4);
+
 	while(true) 
 	{
-		printf("Hello world from Mbed CE!\n");
-		ThisThread::sleep_for(1s);
+		printf("Hello world from Mbed CE! %d\n", cnt++);
+		main_led = 1;
+		ThisThread::sleep_for(250ms);
+		main_led = 0;
+		red_led = 1;
+		ThisThread::sleep_for(250ms);
+		red_led = 0;
+		green_led = 1;
+		ThisThread::sleep_for(250ms);
+		green_led = 0;
+		blue_led = 1;
+		ThisThread::sleep_for(250ms);
+		blue_led = 0;
 	}
 
 	// main() is expected to loop forever.
---
+++ variant.cpp
@@ -0,0 +1,39 @@
+#include "mbed.h"
+#include "nrf.h"
+#include "nrf_rtc.h"
+#include "nrf_uarte.h"
+#include "nrf_uart.h"
+
+I2C_PULLUP = p32,
+
+void initVariant() {
+  // turn power LED on
+  DigitalOut(LED_POWER, 1);
+
+  // Errata Nano33BLE - I2C pullup is controlled by the SWO pin.
+  // Configure the TRACEMUX to disable routing SWO signal to pin.
+  NRF_CLOCK->TRACECONFIG = 0;
+
+  // FIXME: bootloader enables interrupt on COMPARE[0], which we don't handle
+  // Disable it here to avoid getting stuck when OVERFLOW irq is triggered
+  nrf_rtc_event_disable(NRF_RTC1, NRF_RTC_INT_COMPARE0_MASK);
+  nrf_rtc_int_disable(NRF_RTC1, NRF_RTC_INT_COMPARE0_MASK);
+
+  // FIXME: always enable I2C pullup and power @startup
+  // Change for maximum powersave
+  DigitalOut(VDD_ENV, 1);
+  DigitalOut(I2C_PULLUP, 1);
+
+  // Disable UARTE0 which is initially enabled by the bootloader
+  nrf_uarte_task_trigger(NRF_UARTE0, NRF_UARTE_TASK_STOPRX); 
+  while (!nrf_uarte_event_check(NRF_UARTE0, NRF_UARTE_EVENT_RXTO)) ; 
+  NRF_UARTE0->ENABLE = 0; 
+  NRF_UART0->ENABLE = 0; 
+
+  NRF_PWM_Type* PWM[] = {
+    NRF_PWM0, NRF_PWM1, NRF_PWM2
+#ifdef NRF_PWM3
+    ,NRF_PWM3
+#endif
+  };
+}
